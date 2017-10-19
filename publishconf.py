21d688f380f5bd297476adf74c542095c2b0d6c2
kate reeher <kreeher@four51.com>
2017-10-19 16:34:51 -0500

    adjusting site url??

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://klreeher.github.io/wedding-pelican/'
+SITEURL = 'https://klreeher.github.io/wedding-pelican'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
