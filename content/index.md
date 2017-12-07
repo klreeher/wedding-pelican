01ccc238ea84de9e23ad6530d0d5b1c3eca25ffb
kate reeher <k.reeher@protonmail.com>
2017-12-07 16:36:46 -0600

    re-gen life

--- content/index.md
+++ content/index.md
@@ -5,6 +5,7 @@ Summary: We're getting married!
 save_as: index.html
 
 
+
 Welcome! We're so glad you're here. On our website, you'll find information about all the festivities around our wedding, including times and dates, as well as locations and access information.
 
 We're so honored that you'll be celebrating this with us!
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,17 +4,8 @@ from __future__ import unicode_literals
 
 TIMEZONE = "America/Chicago"
 DEFAULT_DATE_FORMAT = '%a %d %B %Y'
-
-# THEME
-THEME = 'pelican-themes/graymill'
-# basic, bootlex, cebong, graymill, monospace,
-#SITETAGLINE = 'Join Us September 15, 2018'
-
-
-AUTHOR = u'Kate'
-SITENAME = u'Reeher-Palmer Wedding'
-SITEURL = 'www.wedding.reeher-palmer.net'
-# contact
+PATH = 'content'
+DEFAULT_LANG = u'en'
 
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = [
@@ -25,10 +16,18 @@ PLUGINS = [
         #'pelican-page-order'
 ]
 
-# PLUGIN CONFIG
+# THEME
+THEME = 'pelican-themes/graymill'
+# basic, bootlex, cebong, graymill, monospace,
 
-## Google Embed
-GMAPS_KEY = 'AIzaSyCUihKCI4VNe-FREn1hgfO9U98vGrhFLy0'
+# Site Info
+AUTHOR = u'Kate'
+SITENAME = u'Reeher-Palmer Wedding'
+SITEURL = 'www.wedding.reeher-palmer.net'
+#SITETAGLINE = 'Join Us September 15, 2018'
+
+
+# PLUGIN CONFIG
 
 ## Autopages
 AUTHOR_PAGE_PATH = 'authors'
@@ -37,12 +36,7 @@ TAG_PAGE_PATH = 'tags'
 
 ## Filetime From Git
 
-
-PATH = 'content'
-
-TIMEZONE = 'America/Chicago'
-
-DEFAULT_LANG = u'en'
+GIT_FILETIME_FROM_GIT = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -52,11 +46,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # theme
-DISPLAY_PAGES_ON_MENU = False
-MENUITEMS = (('Home', "/"), 
-	('Wedding Ceremony','/pages/ceremony.html'),
-	('Wedding Reception','/pages/reception.html'),
-	('Registry','/pages/wedding-registry.html'))
+DISPLAY_PAGES_ON_MENU = True
 
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_SUMMARY = True
@@ -66,8 +56,8 @@ DEFAULT_PAGINATION = 5
 MAIL = 'wedding@reeher-palmer.net'
 DIRECT_TEMPLATES = ['index']
 
-COPYRIGHT = 'Copyright 2017 KLReeher'
+COPYRIGHT = 'Copyright 2017 Reeher-Palmer'
 SHOW_COPYRIGHT = True
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+# RELATIVE_URLS = True
